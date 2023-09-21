530095638e08b9f7e70da04b38cc5d4d334caf9c
Diana Gakuya <deegakuya@gmail.com>
2023-09-21 23:02:33 +0300

    Assignment 3

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+// Function to create 'thank you' cards
+function writeCards(names, eventName) {
+    const thankYouCards = [];
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+      thankYouCards.push(message);
+    }
+    return thankYouCards;
+  }
+  
+  // Function to count down from a positive integer
+  function countDown(startingNumber) {
+    while (startingNumber >= 0) {
+      console.log(startingNumber);
+      startingNumber--;
+    }
+  }
+  
+  
